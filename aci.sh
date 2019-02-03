6bb4cac37f3a33c8377c8b428b3e85d72c3701a0
Simona Cotin <simona.cotin@gmail.com>
2019-02-03 13:07:16 -0500

    add command to get db key

--- aci.sh
+++ aci.sh
@@ -1,3 +1,5 @@
 az group create --location westus --name ignitethetour
 
 az storage account show-connection-string -g tailwind -n tailwindfnstorage30
+
+az cosmosdb list-keys --resource-group tacos --name tacos
